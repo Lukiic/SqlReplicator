65cdf30189da853e311fc7126177e1dc5700a08d
Igor Lukic <igorlukic2003@gmail.com>
2025-02-13 13:43:29 +0100

    Add FileImportService for reading file data

--- Application/Program.cs
+++ Application/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+using SQLReplicator.Services.FileServices;
 using SQLReplicator.Services.LoggerServices;
 using System.Reflection.Metadata.Ecma335;
 
@@ -9,9 +10,34 @@ namespace SQLReplicator.Application
         static void Main(string[] args)
         {
             LoggerService.InitializeLogger();
+            Log.Information("Application has started.");
 
-            
+            #region ValidatingApplicationArguments
+            if (args.Length != 1)
+            {
+                Log.Error("Wrong number of application arguments. Expected: filePath");
+                return;
+            }
+            string filePath = args[0];
+            #endregion
 
+            #region ValidatingFileData
+            List<string> dataFromFile = FileImportService.LoadData(filePath);
+
+            if (dataFromFile.Count < 4)
+            {
+                Log.Error("File doesn't include necessary data. Expected: Two SQL connection strings, Last change version (default 0), Table name");
+                return;
+            }
+
+            string connectionToServer1 = dataFromFile[0];
+            string connectionToServer2 = dataFromFile[1];
+            string lastChangeVersion = dataFromFile[2];
+            string tableName = dataFromFile[3];
+            #endregion
+
+
+            Log.Information("Application has finished replication.");
             LoggerService.CloseLogger();
         }
     }
---
+++ Services/FileServices/FileImportService.cs
@@ -0,0 +1,28 @@
+﻿using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLReplicator.Services.FileServices
+{
+    public class FileImportService
+    {
+        public static List<string> LoadData(string path)
+        {
+			List<string> importedLines;
+
+			try
+			{
+                importedLines = File.ReadAllLines(path).ToList();
+			}
+			catch (Exception)
+			{
+                importedLines = new List<string>();	
+			}
+
+			return importedLines;
+        }
+    }
+}
