db035336962e5a53cf96d6a97e0577204d89f438
Igor Lukic <igorlukic2003@gmail.com>
2025-02-25 11:57:42 +0100

    Add unit tests for CreateTriggerService and UpdateChangeTrackingTableService

---
+++ SQLReplicator.Tests/Services/ChangeTrackingServices/CreateTriggerServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using SQLReplicator.Domain.Services;
+using SQLReplicator.Services.ChangeTrackingServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLReplicator.Tests.Services.ChangeTrackingServices
+{
+    public class CreateTriggerServiceTests
+    {
+        private CreateTriggerService _createTriggerService;
+        Mock<IExecuteSqlCommandService> executeSqlCommandMock;
+
+        public CreateTriggerServiceTests()
+        {
+            executeSqlCommandMock = new Mock<IExecuteSqlCommandService>();
+        }
+
+        [Fact]
+        public void CreateTrigger_ReturnsTrueIfThereIsNoException()
+        {
+            // Arrange
+            executeSqlCommandMock.Setup(x => x.ExecuteCommand(It.IsAny<string>()));
+
+            _createTriggerService = new CreateTriggerService(executeSqlCommandMock.Object);
+
+            // Act
+            bool retVal = _createTriggerService.CreateTrigger("TableName", new List<string>());
+
+            // Assert
+            Assert.True(retVal);
+        }
+
+        [Fact]
+        public void CreateTrigger_ReturnsFalseIfThereIsException()
+        {
+            // Arrange
+            executeSqlCommandMock.Setup(x => x.ExecuteCommand(It.IsAny<string>())).Throws<Exception>();
+
+            _createTriggerService = new CreateTriggerService(executeSqlCommandMock.Object);
+
+            // Act
+            bool retVal = _createTriggerService.CreateTrigger("TableName", new List<string>());
+
+            // Assert
+            Assert.False(retVal);
+        }
+    }
+}
---
+++ SQLReplicator.Tests/Services/ChangeTrackingServices/UpdateChangeTrackingTableServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Moq;
+using SQLReplicator.Domain.Services;
+using SQLReplicator.Services.ChangeTrackingServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLReplicator.Tests.Services.ChangeTrackingServices
+{
+    public class UpdateChangeTrackingTableServiceTests
+    {
+        private UpdateChangeTrackingTableService _updateChangeTrackingTable;
+        Mock<IExecuteSqlCommandService> executeSqlCommandMock;
+
+        public UpdateChangeTrackingTableServiceTests()
+        {
+            executeSqlCommandMock = new Mock<IExecuteSqlCommandService>();
+        }
+
+        [Fact]
+        public void UpdateReplicatedBit_ReturnsTrueIfThereIsNoException()
+        {
+            // Arrange
+            executeSqlCommandMock.Setup(x => x.ExecuteCommand(It.IsAny<string>()));
+
+            _updateChangeTrackingTable = new UpdateChangeTrackingTableService(executeSqlCommandMock.Object);
+
+            // Act
+            bool retVal = _updateChangeTrackingTable.UpdateReplicatedBit("TableName", "LastChangeID", "ReplicatedBitNum");
+
+            // Assert
+            Assert.True(retVal);
+        }
+
+        [Fact]
+        public void UpdateReplicatedBit_ReturnsFalseIfThereIsException()
+        {
+            // Arrange
+            executeSqlCommandMock.Setup(x => x.ExecuteCommand(It.IsAny<string>())).Throws<Exception>();
+
+            _updateChangeTrackingTable = new UpdateChangeTrackingTableService(executeSqlCommandMock.Object);
+
+            // Act
+            bool retVal = _updateChangeTrackingTable.UpdateReplicatedBit("TableName", "LastChangeID", "ReplicatedBitNum");
+
+            // Assert
+            Assert.False(retVal);
+        }
+    }
+}
